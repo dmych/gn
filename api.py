ed7aae35b84beed7e097792a2f7660e1d8e44b72
dmych <dbrechalov@gmail.com>
2011-05-05 21:08:09 +0400

    Copyright notices and license (GPLv3) added.

--- api.py
+++ api.py
@@ -1,11 +1,25 @@
-# api.py: simplenote API implementation
+# This file is part of Geeky Notes
+# 
+# Geeky Notes is a CLI Simplenote client
+# <https://github.com/dmych/gn>
+# 
+# Copyright (c) Dmitri Brechalov, 2010-2011
+# 
+# Geeky Notes is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Geeky Notes is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
-'''This is a part of gn project
-gn (Geeky Notes) is a CLI Simplenote client
-https://github.com/dmych/gn
-Copyright (c) Dmitri Brechalov, 2010-2011
-
-This code is based on python-simplenote project
+'''Geeky Notes - Simplenote CLI client
+api.py: simplenote API implementation
 '''
 
 import base64
--- db.py
+++ db.py
@@ -1,9 +1,25 @@
-# db.py: local database implementation
+# This file is part of Geeky Notes
+# 
+# Geeky Notes is a CLI Simplenote client
+# <https://github.com/dmych/gn>
+# 
+# Copyright (c) Dmitri Brechalov, 2010-2011
+# 
+# Geeky Notes is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Geeky Notes is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
-'''This is a part of gn project
-gn (Geeky Notes) is a CLI Simplenote client
-https://github.com/dmych/gn
-Copyright (c) Dmitri Brechalov, 2010-2011
+'''Geeky Notes - Simplenote CLI client
+db.py: local database implementation
 '''
 
 import shelve
